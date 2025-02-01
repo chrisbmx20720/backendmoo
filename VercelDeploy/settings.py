600a15e899d045e64f63a933f3b453c9d857c0d3
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-01-31 21:01:32 -0600

    backend

--- VercelDeploy/settings.py
+++ VercelDeploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'https://youtu.be/eQJFHfFn-sk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
