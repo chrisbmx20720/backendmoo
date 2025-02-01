2f57567f7f7e8d20a1db4840fde6e31e99c50564
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-01-31 20:38:16 -0600

    backend

--- VercelDeploy/settings.py
+++ VercelDeploy/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Mis apps
-    'Home.apps.HomeConfig',
+    'api',
 ]
 
 MIDDLEWARE = [
