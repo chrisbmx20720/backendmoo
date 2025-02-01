602146f716ca1af2a9313baa431a0ed5608de7f2
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-01-31 20:54:08 -0600

    backend

--- VercelDeploy/settings.py
+++ VercelDeploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'https://youtu.be/eQJFHfFn-sk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- VercelDeploy/urls.py
+++ VercelDeploy/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Home.urls')),
+    path('api/', include('api.urls')),  # Ruta para la API
 ]
 
 
