df5c892dee72610e367db98abf888a2645fd3019
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-01-31 23:32:14 -0600

    backend

--- Home/urls.py
+++ Home/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+from .views import LeerArchivoPdf
+
+urlpatterns=[
+    path('procesar-pdf/', LeerArchivoPdf.as_view(), name='procesar_pdf')
+]
+
+'''
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+'''
--- VercelDeploy/settings.py
+++ VercelDeploy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'https://youtu.be/eQJFHfFn-sk'
+SECRET_KEY = 'django-insecure-!vi6ukp852c^-%8x8q$ik4f*b%!7kkcr*y7)w6#ebw59^_ob0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
