1e5d1c0094578d7281937f6173ea0d2d08a93ed0
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-01-31 23:45:17 -0600

    backend

--- Home/urls.py
+++ Home/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path
 from . import views
 
-from .views import LeerArchivoPdf
-
-urlpatterns=[
-    path('procesar-pdf/', LeerArchivoPdf.as_view(), name='procesar_pdf')
-]
-
-'''
 urlpatterns = [
     path('', views.index, name='index'),
 ]
-'''
